6fdadfa139b69a425d4d23a7378dbcba47470f3e
JoSantana0005 <joseasantana05@gmail.com>
2024-10-13 22:08:02 -0400

    Imagenes de los personajes y js

--- Star Wars/Page/app.js
+++ Star Wars/Page/app.js
@@ -8,3 +8,13 @@ Right.addEventListener('click',()=>{
 Left.addEventListener('click',()=>{
     Peliculas.scrollLeft -= 430;
 })
+//Evento para mover los personajes
+let Right_person = document.getElementById('Right--personajes');
+let Left_person = document.getElementById('Left--personajes');
+let Personajes = document.getElementsByClassName('Person')[0];
+Right_person.addEventListener('click',()=>{
+    Personajes.scrollLeft += 430;
+})
+Left_person.addEventListener('click',()=>{
+    Personajes.scrollLeft -= 430;
+})
