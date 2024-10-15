ae16524cbd0e760623fa384ea59873b1fbeac891
JoSantana0005 <joseasantana05@gmail.com>
2024-10-14 22:46:12 -0400

    Prueba de json

--- Star Wars/Page/app.js
+++ Star Wars/Page/app.js
@@ -18,3 +18,25 @@ Right_person.addEventListener('click',()=>{
 Left_person.addEventListener('click',()=>{
     Personajes.scrollLeft -= 430;
 })
+//Json para las imagenes de los personajes
+const Person = [
+    {
+        "Name": "Anakin Skywalker",
+        "Content": "Es un personaje ficticio de la saga de Star Wars. Es un Jedi que se convierte en el villano Darth Vader. Es interpretado por el actor Hayden Christensen en los episodios II y III de la saga.",
+        "Imagen": "https://static.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/revision/latest?cb=20161019014713"
+    },
+    {
+        "Name": "Luke Skywalker",
+        "Content": "Es un personaje ficticio y el protagonista de la trilogía original de Star Wars. Es hijo de Anakin Skywalker y Padmé Amidala, hermano gemelo de Leia Organa y padre de Ben Solo.",
+        "Imagen": "https://static.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg/revision/latest?cb=20171215195636"
+    },
+    {
+        "Name": "Leia Organa",
+        "Content": "Es un personaje ficticio de la saga de Star Wars. Es la hermana gemela de Luke Skywalker, hija de Anakin Skywalker y Padmé Amidala, y madre de Ben Solo.",
+        "Imagen": "https://static.wikia.nocookie.net/starwars/images/1/1e/Leia_Organa_TROS.png/revision/latest?cb=20200118190126"
+    },
+    {
+        "Name": "Han Solo",
+        "Content": "Es un personaje ficticio de la saga de Star Wars. Es un contrabandista y héroe de la Alianza Rebelde. Es interpretado por Harrison Ford en la trilogía original de Star Wars.",
+    }
+]
