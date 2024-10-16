91c106074d7d0d057b615e4d987d246e326dd819
JoSantana0005 <joseasantana05@gmail.com>
2024-10-16 11:45:59 -0400

    Ruta a la principal

--- Star Wars/Login/login.js
+++ Star Wars/Login/login.js
@@ -15,3 +15,41 @@ correo.addEventListener('input',()=>{
         }
     }
 })
+let contraseña = document.getElementById('Contraseña');
+contraseña.addEventListener('input',()=>{
+    if(contraseña.value == 0){
+        contraseña.style = "border-right: 2px solid ##ffe81f; border-bottom: 2px solid ##ffe81f;"
+    }else{
+        if((contraseña.value.length >= 8 && contraseña.value.length <= 16)){
+            contraseña.style = "border-right: 2px solid #0f0; border-bottom: 2px solid #0f0;";
+        }else{
+            contraseña.style = "border-right: 2px solid #f00; border-bottom: 2px solid #f00";
+        }
+    }
+})
+//Evento de los botones
+const enlace = '../Page/Index.html';
+let Ingresar = document.getElementById('Ingresar');
+let CrearCuenta = document.getElementById('Cuenta');
+let lista = [];
+const ListaUsario = {
+    Correo: '',
+    Contraseña: ''
+};
+Ingresar.addEventListener('click',()=>{
+    if(validarCorreo(correo.value) && contraseña.value.length >= 8 && contraseña.value.length <= 16){
+        ListaUsario.Correo = correo.value;
+        ListaUsario.Contraseña = contraseña.value;
+        if(enlace){
+            alert('Ingreso con exito')
+            window.Location.href = enlace
+        }else{
+            console.log('No se encontro la pagina');
+        }
+        
+    }else if(correo.value == 0 && contraseña.value == 0){
+        alert('Complete los campos para poder ingresar')
+    }else if(correo.value == 0 || contraseña.value == 0){
+        alert('Hay algunos campos vacios, por favor complete todos los campos para poder continuar');
+    }
+})
