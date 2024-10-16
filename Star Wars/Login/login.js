7773a35b234678e787330c95b8263e3e4e4e3426
JoSantana0005 <joseasantana05@gmail.com>
2024-10-16 11:00:05 -0400

    Validación de correo

--- Star Wars/Login/login.js
+++ Star Wars/Login/login.js
@@ -1,6 +1,17 @@
 //Evento que valida el correo y la contraseña
 function validarCorreo(Gmail){
     const regex = /^[a-zA-Z0-9_.+-]+@gmail.com$/;
-    return regex.test(gmail)
+    return regex.test(Gmail)
 }
 let correo = document.getElementById('Correo');
+correo.addEventListener('input',()=>{
+    if(correo.value == 0){
+        correo.style = "border-right: 2px solid ##ffe81f; border-bottom: 2px solid ##ffe81f;"
+    }else{
+        if(validarCorreo(correo.value)){
+            correo.style = "border-right: 2px solid #0f0; border-bottom: 2px solid #0f0;";
+        }else{
+            correo.style = "border-right: 2px solid #f00; border-bottom: 2px solid #f00";
+        }
+    }
+})
